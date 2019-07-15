50c260a0edcc6124a0aca05c61e5476af310f1c0
CalvinSey <40975050+ChepChanSeyha@users.noreply.github.com>
2019-07-15 17:15:36 +0700

    login

---
+++ lib/login/page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+
+class Page extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return LoginState();
+  }
+
+}
