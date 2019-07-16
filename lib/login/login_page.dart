eb9fade7edd46ac41ce759524d06e751bd33bc14
CalvinSey <40975050+ChepChanSeyha@users.noreply.github.com>
2019-07-16 09:18:14 +0700

    login screen

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -6,21 +6,100 @@ class LoginState extends State<Page> {
   Widget build(BuildContext context) {
     // TODO: implement build
 
-    final bg = _background();
-
     return Scaffold(
-      body: bg,
+      body: _background(),
     );
   }
 }
 
 Widget _background() {
   return Container(
-    decoration: BoxDecoration(
-      image: DecorationImage(
-        image: AssetImage('lib/images/login.png'),
-        fit: BoxFit.cover
+      constraints: new BoxConstraints.expand(
+        height: 900.0,
       ),
-    ),
-  );
+      alignment: Alignment.topCenter,
+      padding: EdgeInsets.only(top: 90, bottom: 450),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('lib/images/login.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Column(
+        children: <Widget>[
+          Expanded(
+              child: Column(
+            children: <Widget>[
+              Expanded(
+                  child: Text(
+                'PROMOBOOK',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40,
+                    color: Colors.white),
+              )),
+              Expanded(
+                  child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      'Accuracy',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17,
+                          color: Colors.white),
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      'Convenience',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17,
+                          color: Colors.white),
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      'Save',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17,
+                          color: Colors.white),
+                    ),
+                  ),
+                ],
+              )),
+            ],
+          )),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                    child: Text(
+                  'Sign In',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.orange),
+                )),
+                Expanded(
+                    child: Text(
+                  'Sign Up',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.white),
+                )),
+              ],
+            ),
+          )
+        ],
+      ));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -11,5 +11,4 @@ class MyApp extends StatelessWidget {
       home: Page(),
     );
   }
-
 }
